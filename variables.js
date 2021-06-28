b8d005c567ee3468dddf04402fa03d366d0cd82d
Nurha <nurhacode@gmail.com>
2021-06-28 14:03:48 +0200

    Added variables exercise which prints the type of variable it is

---
+++ variables.js
@@ -0,0 +1,23 @@
+// Create and print a variable for each type:
+
+// - Boolean variable called isWeekend set it to 'false'
+// - Number variable called theNumber that is bigger than 37
+// - String variable called theMessage that is longer than 11 characters
+// - Create a new Date variable called theDate - use new Date() to create a Date variable.
+// - Create a List called bootCampers with at least 3 bootcamper's names in it.
+
+var isWeekend = false;
+console.log(typeof isWeekend);
+
+var theNumber = 39;
+console.log( typeof theNumber);
+
+var theMessage = "This is the message that is a string";
+console.log(typeof theMessage);
+
+var theDate = new Date();
+console.log(typeof theDate);
+
+var bootCampers = ["Suki", "Loki", "Kali", "Yoko"];
+console.log(typeof bootCampers);
+
