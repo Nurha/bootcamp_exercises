bfb833f9f0241261bf6f7e72c94951f3210940fc
Nurha <nurhacode@gmail.com>
2021-06-28 13:45:13 +0200

    Added first function

---
+++ vending_machine_message.js
@@ -0,0 +1,8 @@
+// Create a function called sayThanks, that takes a name of the item selected on a vending machine and returns a message thanking the customer for ordering that item.
+
+function sayThanks(snack) {
+    return 'Thank you for buying a ' + snack;
+}
+
+sayThanks("Bar One");
+console.log(sayThanks("Bar One"));
